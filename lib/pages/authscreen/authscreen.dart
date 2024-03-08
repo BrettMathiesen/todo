77c0b58afcd2317133870ccd5711afa9a1f6dd7e
Brett Mathiesen <brett_math@hotmail.com>
2024-03-08 07:56:28 -0800

    Firebase changes

--- lib/pages/authscreen/authscreen.dart
+++ lib/pages/authscreen/authscreen.dart
@@ -37,11 +37,51 @@ class _authScreenState extends State<AuthScreen> {
                   height: 10,
                 ),
                 TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    final bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+                    if (!emailValid) {
+                      return "Enter valid email address";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter Email",
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
                   keyboardType: TextInputType.visiblePassword,
                   obscureText: true,
+                  validator: (value) {
+                    final bool validPass = RegExp(
+                            r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                        .hasMatch(value!);
+                    if (!validPass) {
+                      return "Enter valid Password";
+                    }
+                    return null;
+                  },
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Enter Password"),
+                    border: OutlineInputBorder(),
+                    labelText: "Enter Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Login'),
+                  ),
                 )
               ],
             ),
@@ -49,4 +89,3 @@ class _authScreenState extends State<AuthScreen> {
         ));
   }
 }
-
